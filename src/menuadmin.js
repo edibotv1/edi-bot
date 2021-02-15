8e1d922e4cc10c844d86dbc8d8665b8879f7ecec
edibotv1 <edilsondias0008@gmail.com>
2021-02-15 03:24:13 -0300

    first commit

---
+++ src/menuadmin.js
@@ -0,0 +1,33 @@
+const menuadmin = (prefix, pushname) => {
+ return `oiin aqui e o menu dos admins
+ _obs para o bot executar esses comandos e nescessario dar adm para o bot_
+ 
+ ◪ *COMANDO DOS ADMINS*
+ │
+ ├─ ❏ ${prefix}abrirgp
+ ├─ ❏ ${prefix}fechargp
+ ├─ ❏ ${prefix}promover
+ ├─ ❏ ${prefix}rebaixar
+ ├─ ❏ ${prefix}tagall
+ ├─ ❏ ${prefix}tagall2
+ ├─ ❏ ${prefix}tagall3
+ ├─ ❏ ${prefix}tagall4
+ ├─ ❏ ${prefix}tagall5
+ ├─ ❏ ${prefix}adicionar
+ ├─ ❏ ${prefix}banir
+ ├─ ❏ ${prefix}listadmins
+ ├─ ❏ ${prefix}linkgrupo
+ ├─ ❏ ${prefix}sair
+ ├─ ❏ ${prefix}bem-vindo
+ ├─ ❏ ${prefix}nsfw
+ ├─ ❏ ${prefix}nivelamento
+ ├─ ❏ ${prefix}level
+ ├─ ❏ ${prefix}delete
+ ├─ ❏ ${prefix}simih
+ └─ ❏ ${prefix}grupo de proprietários
+ `
+
+
+}
+
+exports.menuadmin = menuadmin
---
+++ src/toinmenu.js
@@ -0,0 +1,12 @@
+const toinmenu = (prefix, pushname) => {
+    return `◪ *Comandos do Toin*
+    │
+    ├─ ❏ ${prefix}setprefix
+    ├─ ❏ ${prefix}bloquear
+    ├─ ❏ ${prefix}bc
+    ├─ ❏ ${prefix}bcgc
+    └─ ❏ ${prefix}limpar tudo
+
+}
+
+exports.toinmenu = toinmenu
